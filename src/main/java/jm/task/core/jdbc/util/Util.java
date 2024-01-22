7e21afb099a190c01db56d5437f6c16bd7136df0
Andersone1347 <panov.deadman@yandex.ru>
2024-01-22 16:33:43 +0300

    Connection it's okay

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    public static Connection getConnection(){
-        Connection connection = null;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
 
+
+    public static Connection getConnection(){
+
+        Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306",
-                    "root","root");
+            connection = DriverManager.getConnection(DB_URL,
+                    DB_USER,DB_PASSWORD);
+            System.out.println("conection OK");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            System.out.println("conection Error");
         }
         return connection;
+
     }
 }
